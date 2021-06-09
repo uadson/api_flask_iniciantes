b68e5aac2202bddc1617887cd16dd6ec82036291
Uadson <uadsoncastro@gmail.com>
2021-06-09 15:11:30 -0300

    método put e delete

--- application.py
+++ application.py
@@ -2,6 +2,7 @@
 motores AA. 
 Disponível em: https://ichi.pro/pt/desenvolvimento-de-api-com-flask-para-iniciantes-178216365709514
 
+Obs.: fundamental utilizar uma ferramente de teste como Postman
 As informações necessárias para coleta serão:
 
 	Number Plate
@@ -25,23 +26,23 @@ buses = [
 
 	{
 
-		'number_plate': 'NAX 123',
-		'manufacturer': 'Toyota',
-		'model': 'Hiace',
-		'year': '2009',
-		'capacity': 18
+		"number_plate": "NAX 123",
+		"manufacturer": "Toyota",
+		"model": "Hiace",
+		"year": "2009",
+		"capacity": 18
 	},
 	{
 
-		'number_plate': 'LX Z19',
-		'manufacturer': 'Ford',
-		'model': 'FordX',
-		'year': '2010',
-		'capacity': 45
+		"number_plate": "LX Z19",
+		"manufacturer": "Ford",
+		"model": "FordX",
+		"year": "2010",
+		"capacity": 45
 	}
 ]
 
-# GET
+# LENDO/EXIBINDO VALORES - método GET
 # Ex.: a função get_buses retorna através da função jsonify(buses)
 # todos os elementos da lista como um objeto JSON
 @app.route('/buses')
@@ -57,18 +58,26 @@ def get_bus(index):
 	bus = buses[index]
 	return jsonify(bus)
 
-# POST
-# Suponha que os dados abaixo devem ser incluídos na lista:
+# ADICIONANDO DADOS - método POST
+# Os dados abaixo serão incluídos na lista buses:
 
-# manutenção...
+"""	{
 
+		"number_plate": "LXA 18",
+		"manufacturer": "Honda",
+		"model": "Honda H",
+		"year": "2018",
+		"capacity": 16
+	}
+"""
+# a variável bus receberá estes dados e será incluída à lista buses
 @app.route('/buses', methods=['POST'])
 def add_bus():
 	bus = request.get_json()
 	buses.append(bus)
 	return bus
 
-# UPDATE
+# UPDATE - método PUT
 # Na url deve ser informado um valor que corresponda ao índice de um elemento na lista
 # Ex.: /buses/1
 # A variável bus_to_update receberá os valores da lista
@@ -81,6 +90,16 @@ def update_bus(index):
 	bus_to_update = request.get_json()
 	buses[index] = bus_to_update
 	return jsonify(buses[index])
+
+# EXCLUINDO DADOS - métod DELETE
+# Na url deve ser informado um valor que corresponda ao índice de um elemento na lista
+# Ex.: /buses/1
+# A função delete_bus(index): receberá o valor do índice informado na url
+# A variável deleted receberá a lista com o método pop e o índice do item que será excluído da lista
+@app.route('/buses/<int:index>', methods=['DELETE'])
+def delete_bus(index):
+	deleted = buses.pop(index)
+	return jsonify(deleted)
 
 if __name__ == '__main__':
 	app.run(debug=True)
